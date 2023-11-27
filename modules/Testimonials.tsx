224da65e57143011e2f9fd1e5bb86a362f19005d
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-11-27 17:52:29 +0100

    completed all the layout

--- modules/Testimonials.tsx
+++ modules/Testimonials.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from "react";
 import Image from "next/image";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 
@@ -8,6 +10,23 @@ import Header from "./Header"
 
 
 const Testimonials = () => {
+    const [position, setPosition] = useState({
+        top: "top-0 left-[5%] md:left-[20%]",
+        bottom: "top-[4rem] md:top-[40%] left-[4rem] md:left-[30%] -z-10"
+    })
+
+    const handleUpSwap = () =>{
+        setPosition(_ => {
+            return { bottom: position.top, top: position.bottom }
+        })
+    }
+
+    const handleDownSwap = () =>{
+        setPosition(_ => {
+            return { top: position.bottom, bottom: position.top }
+        })
+    }
+
     return (
         <section className="my-[9rem]">
             <div className="cont grid grid-cols-1 md:grid-cols-[40%_60%]">
@@ -26,7 +45,7 @@ const Testimonials = () => {
 
                 {/* textimonials */}
                 <div className="testimonials relative mt-[6rem] md:mt-0">
-                    <div className="testimony absolute top-0 left-[5%] md:left-[20%] w-[20rem] md:w-[30rem] trans text-slate-600">
+                    <div className={`testimony absolute ${position.top} w-[20rem] md:w-[30rem] trans text-slate-600`}>
                         <div className="image absolute -top-[1rem] -left-[1rem] md:-top-[3rem] md:-left-[2rem]">
                             <Image src={Man} className="w-[3rem] md:w-[5rem] rounded-full" width={20} alt="testifier" unoptimized/>
                         </div>
@@ -36,7 +55,7 @@ const Testimonials = () => {
                             <h6 className="location">Lahore, Pakistan</h6>
                         </div>
                     </div>
-                    <div className="testimony absolute top-[4rem] md:top-[40%] left-[4rem] md:left-[30%] -z-10 w-[20rem] md:w-[30rem] trans text-slate-600">
+                    <div className={`testimony absolute ${position.bottom} w-[20rem] md:w-[30rem] trans text-slate-600`}>
                         <div className="image absolute -top-[1rem] -left-[1rem] md:-top-[3rem] md:-left-[2rem]">
                             <Image src={Man} className="w-[3rem] md:w-[5rem] rounded-full" width={20} alt="testifier" unoptimized/>
                         </div>
@@ -49,8 +68,8 @@ const Testimonials = () => {
 
                     {/* controls */}
                     <div className="absolute -top-[3rem] md:top-[30%] -right-[5%] md:-right-[10%] flex gap-[2rem] flex-col justify-center">
-                        <MdKeyboardArrowUp className="text-[2rem] cursor-pointer"/>
-                        <MdKeyboardArrowDown className="text-[2rem] cursor-pointer"/>
+                        <MdKeyboardArrowUp className="text-[2rem] cursor-pointer" onClick={handleUpSwap}/>
+                        <MdKeyboardArrowDown className="text-[2rem] cursor-pointer" onClick={handleDownSwap}/>
                     </div>
                 </div>
             </div>
